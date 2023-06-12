88557feea7169ed7aff411a50a8b1fb9c54ff238
Tanvi Shah <tanvi.shah.ca@gmail.com>
2023-06-12 08:24:58 -0700

    Initial Commit

---
+++ GitHub Practice/GitHub_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_PracticeApp.swift
+//  GitHub Practice
+//
+//  Created by user242331 on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
